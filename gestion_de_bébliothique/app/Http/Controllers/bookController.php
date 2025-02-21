fef59acdb6cc3630a7a8ac1fce95e9b3be28279d
latifi777 <abdelalilatifi3@gmail.com>
2025-02-21 12:20:25 +0100

    working on books crud

---
+++ gestion_de_bébliothique/app/Http/Controllers/bookController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+use Illuminate\Support\Facades\Storage;
+
+class BookController extends Controller
+{
+    // Afficher tous les livres
+    public function index()
+    {
+        $books = Book::all();
+        return view('admin', compact('books'));
+    }
+   
+    // Enregistrer un nouveau livre
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'author' => 'required|string|max:255',
+            'genre' => 'required|string|max:255',
+            'photo' => 'image|mimes:jpeg,png,jpg,gif|max:2048',
+            'stock' => 'required|integer|min:0',
+        ]);
+
+        // Gestion de l'upload de l'image
+        $photoPath = $request->file('photo') ? $request->file('photo')->store('books', 'public') : null;
+
+        Book::create([
+            'title' => $request->title,
+            'author' => $request->author,
+            'genre' => $request->genre,
+            'photo' => $photoPath,
+            'stock' => $request->stock,
+        ]);
+
+        return redirect()->route('dashboard')->with('success', 'Livre ajouté avec succès !');
+    }
+
+   
+    
+    // Supprimer un livre
+    public function destroy(Book $book)
+    {
+        if ($book->photo) {
+            Storage::disk('public')->delete($book->photo);
+        }
+
+        $book->delete();
+        return redirect()->route('dashboard')->with('success', 'Livre ajouté avec succès !');
+    }
+}
